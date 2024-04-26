53c98afe88526c7197cbfaf6f52ea441b0ea0c9a
Tuhin Ahamed <learnprogramminglangguage@gmail.com>
2024-04-26 19:29:03 +0600

    working with cloudinary

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,5 +9,8 @@ const _config = {
   refresh_token_secret: process.env.REFRESH_TOKEN_SECRET as string,
   refresh_token_expiry: process.env.REFRESH_TOKEN_EXPIRY as string,
   limit:process.env.LIMIT as string,
+  cloudinary_cloud_name:process.env.CLOUDINARY_CLOUD_NAME as string,
+  cloudinary_cloud_api_key:process.env.CLOUDINARY_API_KEY as string,
+  cloudinary_api_secret:process.env.CLOUDINARY_API_SECRET as string,
 };
 export const config= Object.freeze(_config)
