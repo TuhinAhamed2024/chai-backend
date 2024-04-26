1aead884b7cb83ea97d4e411e2ae69270d2eee67
Tuhin Ahamed <learnprogramminglangguage@gmail.com>
2024-04-26 19:39:17 +0600

    create multer middleware function

---
+++ src/middlewares/multer.middleware.ts
@@ -0,0 +1,10 @@
+import multer from 'multer'
+const UploadOnMuter:any = multer.diskStorage({
+  destination:function(req,file,cb){
+    cb(null,"../public/temp")
+  },
+  filename(req, file, callback) {
+    callback(null,file.originalname);
+  },
+})
+export const upload = multer(UploadOnMuter)
